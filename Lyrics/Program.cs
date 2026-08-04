314dc86266189a20eed28409bf9c138775b254fb
Dyala Nafis <dyala.nafees@gmail.com>
2026-08-04 20:39:55 +0300

    Add Song Lyrics Project

---
+++ Lyrics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] lyrics ={
+            "But I feel so seen in the night",
+            "So, for now, it's only me",
+            "And maybe that's all I need."
+        };
+        foreach (string l in lyrics)
+        {
+            for (int i = 0; i < l.Length; i++)
+            {
+                Console.Write(l[i]);
+                Thread.Sleep(55);
+            }
+            Thread.Sleep(1900);
+            Console.WriteLine("");
+        }
+    }
+}
